19b87ee1dc09d654eaf34822d9a49cb4569a7e72
Barne Kleinen <barne.kleinen@htw-berlin.de>
2024-07-10 21:07:07 +0200

    consolidate manifest.js and application.scss

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,8 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .scss
+//= link_directory ../stylesheets/application .scss
+//= link_directory ../stylesheets/application .erb
+//= link_directory ../stylesheets/frontend .scss
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_directory ../fonts
-
---
+++ doc/frontend.md
@@ -0,0 +1,15 @@
+# Guides notes
+
+Console:
+
+Rails.application.config.assets.paths
+
+
+# fonts:
+
+E, [2024-07-10T20:44:31.462537 #39292] ERROR -- :
+ActionController::RoutingError (No route matches [GET] "/webfonts/fa-solid-900.ttf"):
+
+https://docs.fontawesome.com/web/use-with/ruby-on-rails
+
+@import "font-awesome";
